63d5e627e768628f1047e152b10e1fcd6f3c8b63
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2021-04-01 15:06:38 -0300

    Finish version

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import Frase from "../shared/frase.model";
 import { FRASES } from "./frases.mock";
@@ -15,20 +15,54 @@ export class PainelComponent implements OnInit {
   public resposta: string = "";
 
   public rodada: number = 0;
-  public rodadaFrase: Frase;
+  public rodadaFrase: Frase = {
+    fraseEng: "",
+    frasePtBr: ""
+  };
+
+  public progresso: number = 0;
+
+  public tentativas: number = 3;
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
 
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada];
+    this.atualizaRodada();
   }
 
   ngOnInit(): void {
   }
 
+
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value;
   }
 
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = "";
+  }
+
   public verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      this.rodada++;
+
+      this.progresso = this.progresso + (100 / this.frases.length);
+
+      if (this.rodada === this.frases.length) {
+        alert("Você venceu!");
+        this.encerrarJogo.emit("vitoria");
+      }
+
+      this.atualizaRodada();
+    } else {
+      this.tentativas--;
+
+      if (this.tentativas === -1) {
+        alert("Você perdeu todas as tentativas!");
+        this.encerrarJogo.emit("derrota");
+      }
+    }
 
   }
 
